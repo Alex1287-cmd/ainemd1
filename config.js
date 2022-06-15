028f5fa7d37d406f707a3d0c7ca0dafd94085c92
Alex1287-cmd <75934725+Alex1287-cmd@users.noreply.github.com>
2022-06-15 22:08:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@zenzen_bot'
+  var sticker_author = 'Zenz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
